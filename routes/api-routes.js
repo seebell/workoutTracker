cea571a2c74e8af9a30537a6ac6436d6e8efd276
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-07-22 15:16:44 -0400

    created post route; fixed a few bugs

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,14 +1,28 @@
 const { Workout } = require("../models");
 
 module.exports = function (app) {
+
     app.get("/api/workouts", ({ body }, res) => {
         console.log("Get workout route is successful");
         Workout.find({})
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
     });
+
+    app.post("/api/workouts", ({ body }, res) => {
+        console.log("Post is working");
+        console.log({ body });
+        Workout.create(body)
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    })
+
 }
